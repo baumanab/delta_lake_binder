56e50c7f83611bf8d57b063c0524117b4471e4bb
Andrew Bauman <baumanab@gmail.com>
2022-03-18 17:26:02 -0700

    Update startup.sh

--- binder/startup.sh
+++ binder/startup.sh
@@ -6,3 +6,5 @@ export PYSPARK_DRIVER_PYTHON_OPTS='lab'
 pyspark --packages io.delta:delta-core_2.12:1.1.0 \
 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
 --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"
+
+exec "@"
